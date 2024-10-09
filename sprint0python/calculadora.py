b481e27fb77b7795be077b70531a00c386b53ee4
PabloTeix <pteixeirag23@fpcoruna.afundacion.org>
2024-10-09 08:47:15 +0200

    tarea calculadora con comentarios

--- sprint0python/calculadora.py
+++ sprint0python/calculadora.py
@@ -1,20 +1,22 @@
 from operaciones import suma, resta, multiplicacion, division
 
-
 def main():
     while True:
-
+        # Solicita al usuario que introduzca dos números
         n1 = int(input("Introduce el primer numero: "))
         n2 = int(input("Introduce el segundo numero: "))
 
+        # Muestra las opciones de operación disponibles
         print("Elige la operación:")
-        print("1.Suma")
-        print("2.Resta")
-        print("3.Multiplicacion")
-        print("4.Division")
+        print("1. Suma")
+        print("2. Resta")
+        print("3. Multiplicacion")
+        print("4. Division")
 
-        operacion = input("elige opcion")
+        # Solicita al usuario que elija una operación
+        operacion = input("Elige opción: ")
 
+        # Ejecuta la operación seleccionada
         if operacion == '1':
             suma(n1, n2)
         elif operacion == '2':
@@ -24,13 +26,13 @@ def main():
         elif operacion == '4':
             division(n1, n2)
         else:
-            print("opcion no disponible")
+            print("Opción no disponible")
 
+        # Pregunta al usuario si desea realizar otra operación
         continuar = input("¿Deseas realizar otra operación? (s/n): ")
         if continuar.lower() != 's':
             print("Programa finalizado.")
             break
-
 
 if __name__ == "__main__":
     main()
