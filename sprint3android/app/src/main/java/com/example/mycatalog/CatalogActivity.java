170bd2974cf9edde5df8f93b0de69b5fc1b7458e
PabloTeix <pteixeirag23@fpcoruna.afundacion.org>
2024-11-20 10:05:55 +0100

    ejercicio 2

--- sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/CatalogActivity.java
@@ -1,6 +1,9 @@
 package com.example.mycatalog;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +16,19 @@ public class CatalogActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.catalog_activity); // Asegúrate de que el layout sea el correcto
+
+        // Referencia al botón "Navegar al detalle"
+        Button buttonNavegarDetalle = findViewById(R.id.button_navegar_detalle);
+
+        // Establecer el OnClickListener
+        buttonNavegarDetalle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Llamada para iniciar DetailActivity
+                Intent intent = new Intent(CatalogActivity.this, DetailActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
