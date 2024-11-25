48cc50df7ddd7338c4fe25f42b5816b7f161342e
PabloTeix <pteixeirag23@fpcoruna.afundacion.org>
2024-11-25 10:27:22 +0100

    ejercicio 3

--- sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
+++ sprint3android/app/src/main/java/com/example/mycatalog/DetailActivity.java
@@ -1,12 +1,17 @@
 package com.example.mycatalog;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import com.bumptech.glide.Glide;
+import jp.wasabeef.glide.transformations.CropCircleTransformation;
+
 
 public class DetailActivity extends AppCompatActivity {
 
@@ -15,10 +20,13 @@ public class DetailActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.detail_activity);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        ImageView imageView = findViewById(R.id.imageView);
+
+// Usamos Glide para cargar una imagen desde un recurso drawable
+        Glide.with(this)  // 'this' es el contexto (puede ser 'this' en Activity o 'getContext()' en Fragment)
+                .load(R.drawable.imagen1)  // Ruta de la imagen (puede ser una URL o un recurso drawable)
+                .transform(new CropCircleTransformation())
+                .into(imageView);  // Aquí es donde cargamos la imagen en el ImageView
     }
 }
