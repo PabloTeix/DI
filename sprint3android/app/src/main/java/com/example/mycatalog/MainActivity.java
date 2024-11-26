fd87bbe59a290667251c4b21d078be0af8757940
PabloTeix <pteixeirag23@fpcoruna.afundacion.org>
2024-11-26 13:22:25 +0100

    ejercicio 5

---
+++ sprint3android/app/src/main/java/com/example/mycatalog/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.mycatalog;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setupNavegation();
+    }
+
+    private void setupNavegation() {
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottonNvigationView);
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_hostfragment);
+        NavigationUI.setupWithNavController(
+                bottomNavigationView,
+                navHostFragment.getNavController()
+
+        );
+
+    }
+}
